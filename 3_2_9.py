0943a16cba0d941adb1eeb01bab63d50ecc0c38b
1 <m.dannicov@mail.ru>
2021-05-05 23:11:49 +0300

    добавил тестовый файл

---
+++ 3_2_9.py
@@ -0,0 +1,7 @@
+# Устанавливаем функцию
+def test_input_text(expected_result, actual_result):
+    # Проверка, что одно равно другому, или сообщение
+    assert expected_result == actual_result, f"expected {expected_result}, got {actual_result}"
+
+# Запуск установленной функции
+# test_input_text(5, 10)
